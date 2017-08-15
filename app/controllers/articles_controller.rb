81f63db6effd567181a34a968bd1e8203b1be434
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-14 18:31:03 -0600

    finished blog comment feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# http://tutorials.jumpstartlab.com/projects/blogger.html
 class ArticlesController < ApplicationController
 
 include ArticlesHelper
@@ -8,6 +9,8 @@ include ArticlesHelper
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
